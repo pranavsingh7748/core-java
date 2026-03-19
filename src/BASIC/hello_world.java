dad77e8edf13e269b0999e3fd725e0582e72e4b7
Pranav Kumar <pranavsingh7748@gmail.com>
2026-03-19 21:15:59 +0530

    Add Hello World program and update tracker

--- src/BASIC/hello_world.java
+++ src/BASIC/hello_world.java
@@ -8,13 +8,60 @@
 //}
 package BASIC;
 
-public class hello_world {
-    public static void main(String[] args) {
-         int num1 = 5;
-         int num2 = 19;
-         int num3=20;
-         int sum=num1+num2*num3;
-        System.out.println(sum);
+//public class hello_world {
+//    public static void main(String[] args) {
+//         int num1 = 5;
+//         int num2 = 19;
+//         int num3=20;
+//         int sum=num1+num2*num3;
+//        System.out.println(sum);
+//
+//    }
+//}
 
-    }
-}
+
+
+
+
+// //                                  DATA TYPE IN JAVA
+
+//public class hello_world {
+//    public static void main(String[] args) {
+//        int num=1;
+//        byte bt= 127;  // its rang is -128 to 127
+//        short sh=485;
+//        long l=1235l;
+//        float f=3.6f; // float type suffix 'f' or 'F' is required
+//        double d = 2.5;
+//        char ch='w';
+//        char ch1='5';
+//        boolean b=true;
+//
+//    }
+//}
+
+
+//
+////                                  LITERALS
+//
+//
+//
+//public class hello_world {
+//    public static void main(String[] args) {
+//        int a=0b101;  // binary form
+//        int b=0100; // octal form
+//        int c=0x6E; // hexadecimal form
+//
+//        float d=101.230f;
+//        float e=0123.222f; //It is a decimal literal despite the leading zero
+//
+//        double f=56;
+//        double g=0x1.8p3; // 1.8 (hex) × 2^3 = 12.0
+//        double h=12e10; //  12 x 10^10 = 1.2E11 : 1.2 x 10^11 (120 billions)
+//
+//        char i='a';
+//        i++;
+//
+//        System.out.println(i);
+//    }
+//}
