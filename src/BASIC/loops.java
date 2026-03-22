b7252c664646b8726a161b4ad87d74e66fa6ee94
Pranav Kumar <pranavsingh7748@gmail.com>
2026-03-22 22:47:32 +0530

    update loops class

--- src/BASIC/loops.java
+++ src/BASIC/loops.java
@@ -1,7 +1,71 @@
 package BASIC;
 
+//                                                         WHILE LOOP
+
+
+//public class loops {
+//    public static void main(String[] args) {
+//        while (true){
+//            System.out.println("Hii Pranav"); // this print infinite  times
+//        }
+//
+//    }
+//}
+
+
+
+//
+//public class loops {
+//    public static void main(String[] args) {
+//        int i=1;
+//        while(i<=6){ // here is a condition loop start from 1 and last 6
+//            System.out.println(i + " Hii");
+//            i++;
+//        }
+//    }
+//}
+
+
+//                                                      Nested Loops
+
+//
+//public class loops {
+//    public static void main(String[] args) {
+//        int i=1;
+//        while (i<=7){
+//            System.out.println(i +" Hii");
+//            int j=1;
+//            while (j<=3){
+//                System.out.println(j+" Hello");
+//                j++;
+//            }
+//            i++;
+//        }
+//
+//    }
+//}
+
+
+
+
+//                                              I want to print alternate using loop and if else
+
 public class loops {
     public static void main(String[] args) {
+        int i =1;
+        int j=1;
 
+        while(i<=4 || i<=5){
+            if (i<=4){
+                System.out.println(i+" Hii");
+                i++;
+            }if (j<=5){
+                System.out.println(j+" Hello");
+                j++;
+            }
+        }
     }
 }
+
+
+
