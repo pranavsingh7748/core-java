07d712aa52886071c85eb85367c442efbf5ba822
Pranav Kumar <pranavsingh7748@gmail.com>
2026-03-22 22:57:07 +0530

    update loops class

--- src/BASIC/loops.java
+++ src/BASIC/loops.java
@@ -48,24 +48,40 @@ package BASIC;
 
 
 
-//                                              I want to print alternate using loop and if else
+//                                              I want to print alternate using while loop and if else
+//
+//public class loops {
+//    public static void main(String[] args) {
+//        int i =1;
+//        int j=1;
+//
+//        while(i<=4 || i<=5){
+//            if (i<=4){
+//                System.out.println(i+" Hii");
+//                i++;
+//            }if (j<=5){
+//                System.out.println(j+" Hello");
+//                j++;
+//            }
+//        }
+//    }
+//}
 
-public class loops {
-    public static void main(String[] args) {
-        int i =1;
-        int j=1;
 
-        while(i<=4 || i<=5){
-            if (i<=4){
-                System.out.println(i+" Hii");
-                i++;
-            }if (j<=5){
-                System.out.println(j+" Hello");
-                j++;
-            }
-        }
-    }
-}
 
+
+//                                     DO-WHILE LOOP
+//
+//public class loops {
+//    public static void main(String[] args) {
+////        int i =1; // what have when I use i=5 in same conditions lets watch
+//        int i =5; // in this code compiler use loop then condition is i=5; and while say i<=4 so loop check the conditions then print only 5
+//        do {
+//            System.out.println(i+" Hii");
+//            i++;
+//        } while (i<=4);
+//    }
+//}
+//
 
 
